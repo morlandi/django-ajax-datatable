7685388a53e6c22f2b21cf7233aedce7e3357377
Petr Dlouhý <petr.dlouhy@email.cz>
2021-11-19 12:52:38 +0100

    add tox and github test workflow

--- tests/test_settings.py
+++ tests/test_settings.py
@@ -10,20 +10,28 @@ USE_TZ = True
 SECRET_KEY = "77777777777777777777777777777777777777777777777777"
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": ":memory:",
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'postgres',  # Or path to database file if using sqlite3.
+        'USER': 'postgres',  # Not used with sqlite3.
+        'PASSWORD': 'postgres',  # Not used with sqlite3.
+        'HOST': 'localhost',  # Set to empty string for localhost. Not used with sqlite3.
+        'PORT': '5432',  # Set to empty string for default. Not used with sqlite3.
     }
 }
 
 #ROOT_URLCONF = "tests.urls"
 
 INSTALLED_APPS = [
+    "user",
     "django.contrib.auth",
     "django.contrib.contenttypes",
     "django.contrib.sites",
     "ajax_datatable",
 ]
+
+AUTH_USER_MODEL="user.TestUser"
 
 SITE_ID = 1
 
---
+++ tests/user/models.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+test_django-task
+------------
+
+Tests for `django-task` models module.
+"""
+
+from django.contrib.auth.models import AbstractUser
+
+class TestUser(AbstractUser):
+    pass
